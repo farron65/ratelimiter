11a8f0559bdfd6e8058275c6f98583e42e36a11c
farron65 <bezhansn@gmail.com>
2026-07-13 21:26:27 -0400

    Add HTTP handler testing token bucket, return 429 when rate is limited

--- cmd/ratelimiter/main.go
+++ cmd/ratelimiter/main.go
@@ -1,15 +1,33 @@
 package main
 
 import (
-	"github.com/farron65/ratelimiter/tokenbucket"
 	"fmt"
-	// "time"
+	"log"
+	"net/http"
+
+	"github.com/farron65/ratelimiter/tokenbucket"
 )
+
+func checkHandler(tb *tokenbucket.TokenBucket) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if tb.Allow() {
+			fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+		} else {
+			// fmt.Fprintf(w, "Too many requests, wait for token bucket to fill up") 
+			// w.WriteHeader(http.StatusTooManyRequests) <---- This won't work because header must be sent first
+
+			//
+			w.WriteHeader(http.StatusTooManyRequests)
+			fmt.Fprintf(w, "Too many requests, wait for token bucket to fill up")
+		}
+	}
+}
 
 func main() {
 	fmt.Println("Hi")
 
-	tb := tokenbucket.NewTokenBucket(10, 2)
+	tb := tokenbucket.NewTokenBucket(10, 1)
 
-	fmt.Println(tb)
+	http.HandleFunc("/", checkHandler(tb))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
