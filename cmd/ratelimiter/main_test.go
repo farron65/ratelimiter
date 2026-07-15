70c695c67ac57c8cffc7c04b2677b0853263e160
farron65 <bezhansn@gmail.com>
2026-07-14 20:51:27 -0400

    Add concurrency test for tokenbucket.Allow()

--- cmd/ratelimiter/main_test.go
+++ cmd/ratelimiter/main_test.go
@@ -38,3 +38,32 @@ func TestGetBucketConcurrent(t *testing.T) {
 		}
 	}
 }
+
+func TestAllowConcurrencySingleBucket(t *testing.T) {
+	tb := tokenbucket.NewTokenBucket(10, 0)
+	
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+
+	successCount := 0
+
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+
+		go func() {
+			defer wg.Done()
+			b := tb.Allow()
+
+			mu.Lock()
+			defer mu.Unlock()
+			if b {successCount ++}
+			
+		}()
+	}
+
+	wg.Wait()
+
+	if successCount != 10 {
+		t.Errorf("Expected %d, instead got %d", 10, successCount)
+	}
+}
